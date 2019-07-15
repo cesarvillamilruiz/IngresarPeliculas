0859b1d16eef5e20b63c6799bc7724c144ebaeb1
cesarvillamilruiz <cesar.hades77@gmail.com>
2019-07-14 19:18:42 -0500

    commit-1

---
+++ plantilla/Controllers/PeliculasController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using plantilla.Models;
+
+namespace plantilla.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private plantillaContext db = new plantillaContext();
+
+        // GET: Peliculas
+        public ActionResult Index(string actor,string gendre)
+        {
+            var gendreList = new List<string>();
+            var gendreQry = from d in db.Peliculas
+                            orderby d.Genero
+                            select d.Genero;
+
+            gendreList.AddRange(gendreQry.Distinct());
+            ViewBag.gendre = new SelectList(gendreList);
+
+            var peli = from m in db.Peliculas
+                       select m;
+
+            if (!String.IsNullOrEmpty(actor))
+            {
+                peli = peli.Where(s => s.Protagonista.Contains(actor));
+            }
+
+            if (!String.IsNullOrEmpty(gendre))
+            {
+                peli = peli.Where(x => x.Genero == gendre);
+            }
+            return View(peli);
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PeliculaId,Nombre,duracion,Genero,FechaDeEstreno,Sinopsis,protagonista")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peliculas.Add(pelicula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PeliculaId,Nombre,duracion,Genero,FechaDeEstreno,Sinopsis,protagonista")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pelicula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pelicula pelicula = db.Peliculas.Find(id);
+            db.Peliculas.Remove(pelicula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ plantilla/Models/Pelicula.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace plantilla.Models
+{
+    public class Pelicula
+    {
+        [Key]
+        public int PeliculaId {get;set;}
+
+        [StringLength(20)]
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        public string Nombre { get; set; }
+
+        [StringLength(10)]
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        public string Duracion { get; set; }
+
+        [StringLength(15)]
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        public string Genero { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        [DataType(DataType.Date)]
+        public DateTime FechaDeEstreno { get; set; }
+
+        [StringLength(200)]
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        public string Sinopsis { get; set; }
+
+        [Required(ErrorMessage = "Este campo es  requerido")]
+        public string Protagonista { get; set; }
+
+    }
+}
